0fcadae7b70fc6b65ac010209308dd1fa593f3d6
Alex Coronell <alexcoronell@gmail.com>
2024-09-10 21:06:35 -0500

    Java Spring Data and Databases course completed

---
+++ 08_java_spring_data/platzi-pizzeria/src/main/java/com/platzi/pizzeria/service/PizzaService.java
@@ -0,0 +1,92 @@
+package com.platzi.pizzeria.service;
+
+import com.platzi.pizzeria.persistence.entity.PizzaEntity;
+import com.platzi.pizzeria.persistence.repository.PizzaPagSortRepository;
+import com.platzi.pizzeria.persistence.repository.PizzaRepository;
+import com.platzi.pizzeria.service.dto.UpdatePizzaPriceDto;
+import com.platzi.pizzeria.service.exception.EmailApiException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class PizzaService {
+    private final PizzaRepository pizzaRepository;
+    private final PizzaPagSortRepository pizzaPagSortRepository;
+
+    @Autowired
+    public PizzaService(PizzaRepository pizzaRepository, PizzaPagSortRepository pizzaPagSortRepository) {
+        this.pizzaRepository = pizzaRepository;
+        this.pizzaPagSortRepository = pizzaPagSortRepository;
+    }
+
+    public Page<PizzaEntity> getAll(int page, int elements) {
+        //return pizzaRepository.findAll();
+        Pageable pageRequest = PageRequest.of(page, elements);
+        return this.pizzaPagSortRepository.findAll(pageRequest);
+    }
+
+    public Page<PizzaEntity> getAvailable(int page, int elements, String sortBy, String sortDirection) {
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        //Pageable pageRequest = PageRequest.of(page, elements, Sort.by(sortBy));
+        Pageable pageRequest = PageRequest.of(page, elements, sort);
+        return pizzaPagSortRepository.findByAvailableTrue(pageRequest);
+    }
+
+    public int getVeganCount(){
+        return this.pizzaRepository.countByVeganTrue();
+    }
+
+    public PizzaEntity getByName(String name) {
+        return this.pizzaRepository.findAllByAvailableTrueAndNameIgnoreCase(name);
+    }
+
+    public PizzaEntity getFirstAvailableByName(String name) {
+        return pizzaRepository.findFirstByAvailableTrueAndNameIgnoreCase(name).orElseThrow(() -> new RuntimeException("La pizza no existe"));
+    }
+
+    public List<PizzaEntity> getWith(String ingredient) {
+        return this.pizzaRepository.findAllByAvailableTrueAndDescriptionContainingIgnoreCase(ingredient);
+    }
+
+    public List<PizzaEntity> getWithout(String ingredient) {
+        return this.pizzaRepository.findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(ingredient);
+    }
+
+    public List<PizzaEntity> getCheapest(double price) {
+        return this.pizzaRepository.findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(price);
+    }
+
+    public PizzaEntity get(int idPizza){
+        return pizzaRepository.findById(idPizza).orElse(null);
+    }
+
+    public PizzaEntity save(PizzaEntity pizza) {
+        return this.pizzaRepository.save(pizza);
+    }
+
+    public Boolean exist(int idPizza) {
+        return this.pizzaRepository.existsById(idPizza);
+    }
+
+    public void delete(int idPizza) {
+        this.pizzaRepository.deleteById(idPizza);
+    }
+
+    @Transactional(noRollbackFor = EmailApiException.class, propagation = Propagation.REQUIRED)
+    public void updatePrice(UpdatePizzaPriceDto dto) {
+        this.pizzaRepository.updatePrice(dto);
+        this.sendEmail();
+    }
+
+    private void sendEmail() {
+        throw new EmailApiException();
+    }
+}
